24b377d5fd804906f539e6d5adff8696b7173bce
Thomas Inagamov <inagamov.tyoma@gmail.com>
2022-07-23 12:41:57 +0300

    Loader for requests added

--- client/src/components/Sheet.jsx
+++ client/src/components/Sheet.jsx
@@ -3,15 +3,29 @@ import React, {useContext} from "react";
 import {observer} from "mobx-react-lite";
 
 // Components imports
+import Loader from "../components/Loader";
 import SheetColumns from "./SheetColumns";
 import SheetRow from "./SheetRow";
 
 // Other imports
 import {Context} from "..";
 
-const Sheet = observer(() => {
+const Sheet = observer(({loading}) => {
 	// Retrieve the context
 	const {sheet} = useContext(Context);
+
+	// While loading, display the loader
+	while (loading) {
+		return (
+			<div className="sheet">
+				{/* Columns */}
+				<SheetColumns></SheetColumns>
+
+				{/* Loader */}
+				<Loader></Loader>
+			</div>
+		);
+	}
 
 	return (
 		<div className="sheet">
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,30 +1,39 @@
 // Module imports
 import {observer} from "mobx-react-lite";
-import React, {useContext, useEffect} from "react";
+import React, {useState, useContext, useEffect} from "react";
 
 // Requests import
 import {fetchSheetRows, createRandomMultipleSheetRows} from "../http/SheetAPI";
 
 // Components imports
+import MyButton from "../components/UI/MyButton";
+import Filter from "../components/Filter";
 import Sheet from "../components/Sheet";
 import Pagination from "../components/Pagination";
-import Filter from "../components/Filter";
-import MyButton from "../components/UI/MyButton";
 
 // Other imports
 import {Context} from "..";
 
 const Home = observer(() => {
+	// Init loading state to track the load
+	const [loading, setLoading] = useState(true);
+
 	// Retrieve the context
 	const {sheet} = useContext(Context);
 
 	function loadSheet() {
+		// Set loading state to true to show that request is being made
+		setLoading(true);
+
 		// Fetch all sheet rows
-		fetchSheetRows(sheet._page, sheet._limit, sheet._filter).then((data) => {
-			// Use store setters to save received data
-			sheet.setRows(data.rows);
-			sheet.setTotalCount(data.count);
-		});
+		fetchSheetRows(sheet._page, sheet._limit, sheet._filter)
+			.then((data) => {
+				// Use store setters to save received data
+				sheet.setRows(data.rows);
+				sheet.setTotalCount(data.count);
+			})
+			// Set loading state to false to show that request has been completed
+			.finally(() => setLoading(false));
 	}
 
 	// First render
@@ -52,7 +61,7 @@ const Home = observer(() => {
 			<Filter></Filter>
 
 			<h1 className="container__title">Sheet.</h1>
-			<Sheet></Sheet>
+			<Sheet loading={loading}></Sheet>
 			<Pagination></Pagination>
 		</div>
 	);
